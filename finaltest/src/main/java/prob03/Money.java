42f7b0f9e9f16acc2865c987ac8daef0343a8eb4
onion237 <ssafy1121@gmail.com>
2021-09-13 17:58:45 +0900

    Money class - money object cache added

--- finaltest/src/main/java/prob03/Money.java
+++ finaltest/src/main/java/prob03/Money.java
@@ -4,26 +4,45 @@ import java.util.Objects;
 
 public class Money {
 	private final int amount;
+	private static final int CACHE_MAX = 10000;
+	private static Money[] cache = new Money[CACHE_MAX];
 	
 	public Money(int money) {
 		this.amount = money;
 	}
 	/* 코드 작성 */
 	public int getAmount() {return this.amount;}
+	
 	public Money add(Money money) {
-		return new Money(this.amount + money.amount);
+		return createMoney(this.amount + money.amount);
 	}
 
 	public Money minus(Money money) {
-		return new Money(this.amount - money.amount);
+		return createMoney(this.amount - money.amount);
 	}
 
 	public Money multiply(Money money) {
-		return new Money(this.amount * money.amount);
+		return createMoney(this.amount * money.amount);
 	}
 
 	public Money devide(Money money) {
-		return new Money(this.amount / money.amount);
+		return createMoney(this.amount / money.amount);
+	}
+	
+	private Money createMoney(int amount) {
+		if(amount >= 0 && amount < CACHE_MAX) { // 캐싱 대상 범위면
+			if(cache[amount] == null) { //캐시에 저장된 불변객체가 있는지 검사
+				synchronized (cache) {
+					if(cache[amount] == null) { //double check
+						cache[amount] = new Money(amount);
+					}
+				}
+			}
+			
+			return cache[amount];
+		}else {
+			return new Money(amount);
+		}
 	}
 
 	@Override
