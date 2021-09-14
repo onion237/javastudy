245d00c9da325843bb69d977040a909c7965842c
onion237 <ssafy1121@gmail.com>
2021-09-14 10:15:42 +0900

    Generic Printer

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,26 +2,29 @@ package prob01;
 
 public class Printer {
 
-	public void println(String string) {
-		System.out.println(string);
-	}
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+//
+//	public void println(int i) {
+//		// TODO Auto-generated method stub
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		// TODO Auto-generated method stub
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		// TODO Auto-generated method stub
+//		System.out.println(d);
+//	}
+//	public void println(Object o) {
+//		System.out.println(o);
+//	}
 
-	public void println(int i) {
-		// TODO Auto-generated method stub
-		System.out.println(i);
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-
-	public void println(boolean b) {
-		// TODO Auto-generated method stub
-		System.out.println(b);
-	}
-
-	public void println(double d) {
-		// TODO Auto-generated method stub
-		System.out.println(d);
-	}
-	public void println(Object o) {
-		System.out.println(o);
-	}
-
 }
